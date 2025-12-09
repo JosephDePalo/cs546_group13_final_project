6e2c4a4f7da47957d13714f2616c094decdae145
Stephen Hayduk <sjhayduk@icloud.com>
2025-12-09 18:10:21 -0500

    Updated EventRegistrations CRUD Functions
    Final implementation of CRUD functions for EventRegistrations

--- server/controllers/eventreg.controller.js
+++ server/controllers/eventreg.controller.js
@@ -1,11 +1,12 @@
 import EventRegistration from "../models/eventreg.model.js";
 
 // @desc     Create new EventRegistration
-// @access   Private
+// @access   Public
 
 export const newEventRegistation = async (req, res) => {
     try {
-        const {user_id, event_id, title, description} = req.body;
+        const {user_id, event_id} = req.body;
+        let current_date = new Date();
         const existingEventReg = await EventRegistration.findOne({title});
     if (existingEventReg) {
       return res.status(400).json({
@@ -16,8 +17,7 @@ export const newEventRegistation = async (req, res) => {
         const eventReg = await EventRegistration.create({
             user_id,
             event_id,
-            title,
-            description
+            registered_at: current_date
         });
 
         res.json(eventReg);
@@ -27,7 +27,7 @@ export const newEventRegistation = async (req, res) => {
     }
 };
 
-// @desc     Get reports
+// @desc     Get EventRegistration
 // @access   Public
 
 export const getEventRegistration = async (req, res) => {
@@ -45,7 +45,7 @@ export const getEventRegistration = async (req, res) => {
     }
 };
 
-// @desc     Get report
+// @desc     Get EventRegistration
 // @access   Public
 
 export const getAllEventRegistrations = async (req, res) => {
@@ -59,17 +59,13 @@ export const getAllEventRegistrations = async (req, res) => {
 }
 
 
-// @desc     Update report 
-// @access   Private/Admin
+// @desc     Update EventRegistration
 
 export const updateEventRegistration = async (req, res) => {
     try {
         let new_eventReg = req.body;
 
         delete new_eventReg._id;
-        delete new_eventReg.disabled;
-        delete new_eventReg.disabled_at;
-        delete new_eventReg.disabled_by;
 
         const eventReg = await EventRegistration.findById(req.params.id);
         if (!eventReg) {
@@ -87,6 +83,51 @@ export const updateEventRegistration = async (req, res) => {
         res.json(updatedEventReg);
     } catch (err) {
         console.error("Update event registration error:", err.message)
+        res.status(500).json({ message: "Unable to update event registration." });
+    }
+}
+
+// @desc     Delete EventRegistration
+
+export const deleteEventReg = async (req, res) => {
+    try {
+        const eventReg = await EventRegistration.findById(req.params.id);
+    
+        if (!eventReg) {
+            return res.status(404).json({ message: "EventRegistration not found." });
+        }
+    
+        await EventRegistration.deleteOne();
+    
+        res.json(eventReg);
+    } catch (err) {
+        console.error("Delete EventRegistration error:", err.message);
+        res.status(500).json({ message: "Unable to delete EventRegistration" });
+    } 
+}
+
+
+// @desc     Cancel EventRegistration
+
+export const cancelEventReg = async (req, res) => {
+    try {
+        let current_date = new Date();
+        const eventReg = await EventRegistration.findById(req.params.id);
+        if (!eventReg) {
+            return res.status(404).json({ message: "Event registration not found." });
+        }
+
+        const updatedEventReg = await EventRegistration.findByIdAndUpdate(req.params.id, {cancelled: true, cancelled_at: current_date}, {
+            new: true,
+        });
+
+        if (!updatedEventReg) {
+            return res.status(404).json({ message: "Event registration not found." });
+        }
+
+        res.json(updatedEventReg);
+    } catch (err) {
+        console.error("Cancel event registration error:", err.message)
         res.status(500).json({ message: "Unable to update event registration." });
     }
 }
