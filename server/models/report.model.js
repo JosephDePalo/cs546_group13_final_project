0cf0c26e74c0bb21671a978f164d3a3d9a02752b
Stephen Hayduk <sjhayduk@icloud.com>
2025-12-09 18:08:30 -0500

    Final Report Model
    Updated Report model implementation

--- server/models/report.model.js
+++ server/models/report.model.js
@@ -3,64 +3,94 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const reportSchema = new mongoose.Schema(
     {
-        user_id: {
-            required: [true, "Field: user_id is required and cannot be null"],
+        reporter_id: {
+            required: [true, "Field: reporter_id is required and cannot be null"],
             type: ObjectId,
             ref: "Users",
-            description: "id of the user registering for an event"
+            description: "id of the user reporting the event"
         },
-        event_id: {
-            required: [true, "Field: event_id is required and cannot be null"],
-            type: ObjectId,
-            ref: "Events",
-            description: "id of the user registering for an event"
-        },
-        violation: {
-            required: [true, "Field: violation is required and cannot be null"],
+        target_type: {
+            required: [true, "Field: target_type is required and cannot be null"],
             type: String,
             trim: true,
-            match: [
-                /^[a-zA-Z0-9_-]+$/,
-                "Invalid violation format: Only letters, numbers, _, and - allowed",
-            ],
-            description: "Type of violation the user is reporting",
+            description: "event | comment| user"
+        },
+        target_id: {
+            required: [true, "Field: target_id is required and cannot be null"],
+            type: ObjectId,
+            ref: "Events",
+            description: "id of the event being reported"
+        },
+        reason: {
+            required: [true, "Field: reason is required and cannot be null"],
+            type: String,
+            trim: true,
+            maxlength: [50, "Invalid description length: max 50 chars"],
+            description: "Reason for why the report is being made"
         },
         description: {
             type: String,
             default: null,
             trim: true,
-            maxlength: [500, "Invalid description length: max 500 chars"]
+            maxlength: [500, "Invalid description length: max 500 chars"],
+            description: "Detailed explanation provided by the reporter"
         },
-        disabled: {
-            type: Boolean,
-            default: false,
-            description: "Can be toggled by admins to disable reports that have been evaluated.",
-        },
-
-        disabled_at: {
-            type: Date,
-            default: null,
-            description: "The date that a report was disabled.",
-        },
-
-        disabled_reason: {
+        severity: {
+            required: [true, "Field: severity is required and cannot be null"],
             type: String,
             trim: true,
-            default: null,
-            description: "The reason that a report was disabled.",
+            description: "High | Medium | Low"
         },
 
-        disabled_by: {
+        resolution_status: {
+            type: String,
+            default: "pending",
+            description: "Pending, reviewed, dismissed"
+        },
+
+        responding_admin_id: {
             type: ObjectId,
             default: null,
             ref: "Users",
-            description: "The user that disabled the report",
+            description: "id of the admin who handled the report"
         },
+
+        reponding_admin_notes: {
+            type: String,
+            default: null,
+            trim: true,
+            maxlength: [500, "Invalid description length: max 500 chars"],
+            description: "Additional notes from admin"
+        },
+
+        resolution_decision: {
+            type: String,
+            default: null,
+            description: "Warned | Resolved || targed_redacted "
+        },
+        
+        resolved_at: {
+            type: Date,
+            default: null,
+            description: "Timestamp of when the report was marked as resolved."
+        },
+
+        created_at: {
+            type: Date,
+            default: null,
+            description: "Timestamp of when the report was created."
+        },
+
+        updated_at: {
+            type: Date,
+            default: null,
+            description: "Timestamp of when the report was updated."
+        }
     },
     {
     timestamps: true,
     },
 )
 
-const Report = mongoose.model("Event", reportSchema);
+const Report = mongoose.model("Report", reportSchema);
 export default Report;
